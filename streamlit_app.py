8541c5391b1422871414746e641fccfb5fad6999
bhaskareddy652 <125952148+bhaskareddy652@users.noreply.github.com>
2024-12-18 16:14:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 st.title('Machine Learning')
 st.write('This app builds a machine learning models! ')
 df=pd.read_excel("Cleaned_dataset.xlsx")
