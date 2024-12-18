bbf9c0acc66dcbbfe985578266543e165ebe349b
bhaskareddy652 <125952148+bhaskareddy652@users.noreply.github.com>
2024-12-18 16:18:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ import pandas as pd
 import numpy as np
 st.title('Machine Learning')
 st.write('This app builds a machine learning models! ')
-df=pd.read_excel("https://github.com/bhaskareddy652/cluster-deploy/blob/master/Cleaned_dataset.xlsx")
+df=pd.read_excel("Cleaned_dataset.xlsx")
 df
